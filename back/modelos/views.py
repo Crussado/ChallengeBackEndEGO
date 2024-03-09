2c6d0dd916affc2ea23f1defb503e946c7703298
Julio Olivencia <julioolivencia891@gmail.com>
2024-03-09 19:08:11 -0300

    add customs apis

--- back/modelos/views.py
+++ back/modelos/views.py
@@ -1,6 +1,10 @@
-from rest_framework import viewsets, mixins
+from django.http import JsonResponse
+from django.db.models import Q
 
-from .models import Modelo
+from rest_framework import viewsets, mixins, status
+from rest_framework.decorators import list_route
+
+from .models import Modelo, Parte
 from .serializers import ModeloSerializer, ModeloListSerializer
 
 class ModeloViewSet(mixins.RetrieveModelMixin,
@@ -17,3 +21,52 @@ class ModeloViewSet(mixins.RetrieveModelMixin,
 
     def get_serializer_class(self):
         return self.serializers.get(self.action, self.serializer_class)
+
+    @list_route(methods=['get'])
+    def toyota_gazoo_racing(self, request):
+        '''
+           Api para la seccion de navegacion Toyota Gazoo Racing, suponiendo que es un modelo
+           lo devuelve. 
+        '''
+        try:
+            modelo = Modelo.objects.get(marca='Toyota', modelo='Gazoo Racing')
+            data = self.serializer_class(modelo).data
+            response = JsonResponse(data, status=status.HTTP_200_OK)
+        except Modelo.DoesNotExist:
+            response = JsonResponse({'error': 'Modelo inexistente.'}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+        return response
+
+    @list_route(methods=['get'])
+    def toyota_hibridos(self, request):
+        '''
+           Api para la seccion de navegacion Toyota hibridos, devuelve los modelos de Toyota
+           con transmision automatica y manual.
+        '''
+        try:
+            partes = Parte.objects.filter(Q(nombre='Transmision automatica') | Q(nombre='Transmision manual'))
+            if partes.count() != 2:
+                raise Exception
+
+            modelos = Modelo.objects.filter(partes=partes.first()).filter(partes=partes.last())
+            data = self.serializer_class(modelos, many=True).data
+
+            response = JsonResponse(data, status=status.HTTP_200_OK)
+        except Exception as ex:
+            response = JsonResponse({'error': str(ex)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+        return response
+
+    
+    @list_route(methods=['get'])
+    def innovacion(self, request):
+        '''
+           Api para la seccion de navegacion Innovacion, devuelve los ultimos 5 modelos sacados
+           al mercado.
+        '''
+        try:
+            modelos = Modelo.objects.order_by('-anio')[:5]
+
+            data = self.serializer_class(modelos, many=True).data
+            response = JsonResponse(data, status=status.HTTP_200_OK)
+        except Exception as ex:
+            response = JsonResponse({'error': str(ex)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+        return response
--- back/productos_extra/views.py
+++ back/productos_extra/views.py
@@ -1,4 +1,7 @@
-from rest_framework import viewsets, mixins
+from django.http import JsonResponse
+
+from rest_framework import viewsets, mixins, status
+from rest_framework.decorators import list_route
 
 from .models import Extra
 from .serializers import ExtraSerializer
@@ -9,3 +12,19 @@ class ExtraViewSet(mixins.RetrieveModelMixin,
     model = Extra
     queryset = Extra.objects.all()
     serializer_class = ExtraSerializer
+
+    @list_route(methods=['get'])
+    def toyota_mobility_service(self, request):
+        '''
+           Api para la seccion de navegacion Toyota Mobility Service, devuelve los servicios de movilidad
+           de relacionado con Toyota.
+        '''
+        try:
+            servicio_nombre = 'Toyota Mobility'
+            servicios = Extra.objects.filter(tipo=True, nombre__icontains=servicio_nombre)
+            
+            data = self.serializer_class(servicios, many=True).data
+            response = JsonResponse(data=data, status=status.HTTP_200_OK)
+        except Exception as ex:
+            response = JsonResponse({'error': str(ex)}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+        return response
