7d2d710f25e91070bd93e581ce4706a30420d964
Julio Olivencia <julioolivencia891@gmail.com>
2024-03-10 02:37:02 -0300

    documentation

--- back/back/settings.py
+++ back/back/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -44,6 +45,7 @@ REST_FRAMEWORK = {
 }
 
 INSTALLED_APPS = [
+    'drf_yasg',
     'rest_framework',
     'django_filters',
     'modelos.apps.ModelosConfig',
--- back/back/urls.py
+++ back/back/urls.py
@@ -17,11 +17,32 @@ from django.contrib import admin
 from django.urls import path
 from django.conf.urls import url, include
 
+# Documentacion
+from django.urls import re_path
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="Ego Challenge",
+      default_version='v1',
+      description="Back automotriz",
+      terms_of_service="https://www.google.com/policies/terms/",
+      contact=openapi.Contact(email=""),
+      license=openapi.License(name="BSD License"),
+   ),
+   public=True,
+   permission_classes=(permissions.AllowAny,),
+)
+
 urlpatterns = [
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     url(r'', include('concesionario.urls')),
     url(r'', include('productos_extra.urls')),
     url(r'', include('financiacion.urls')),
     url(r'', include('modelos.urls')),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
     path('admin/', admin.site.urls),
 ]
